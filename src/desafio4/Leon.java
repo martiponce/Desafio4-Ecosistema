0f6c1dbac1bc3b9d336391e331a698e028923a7d
Fede-dev25 <fedezamparo@hotmail.com.ar>
2025-09-15 20:16:20 -0300

    ADD: Clase Leon con metodos de caza

---
+++ src/desafio4/Leon.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package desafio4;
+
+public class Leon extends Animal implements HabitatSabana {
+    public Leon(String nombre, double peso) {
+        super(nombre, peso);
+    }
+    // ... (resto del código del león)
+    public void cazarA(Cebra cebra) { ... }
+}
